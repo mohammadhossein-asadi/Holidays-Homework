8673d9c9e6a164a798c23cc4dec8139bbcf064a9
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-03-27 18:49:36 +0430

    add

--- 5-&-6-sass/gulpfile.js
+++ 5-&-6-sass/gulpfile.js
@@ -1,13 +1,14 @@
-const { src, dest, watch, series } = require("gulp");
-
-const sass = require("gulp-sass")(require("sass"));
+const { src, dest, watch, series } = require('gulp')
+const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return src("index.scss").pipe(sass()).pipe(dest("css"));
+  return src('index.scss')
+    .pipe(sass())
+    .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(["index.scss"], buildStyles);
+  watch(['index.scss'], buildStyles)
 }
 
-exports.defult = series(buildStyles, watchTask);
+exports.default = series(buildStyles, watchTask)
