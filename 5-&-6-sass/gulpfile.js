32e521e8f61c8dbe4c3f8f1559755ee5bd9bc14f
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-03-28 12:12:22 +0430

    chabge

--- 5-&-6-sass/gulpfile.js
+++ 5-&-6-sass/gulpfile.js
@@ -1,14 +1,12 @@
-const { src, dest, watch, series } = require('gulp')
-const sass = require('gulp-sass')(require('sass'));
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src('*.scss')
-    .pipe(sass())
-    .pipe(dest('css'))
+  return src("shinobi/*.scss").pipe(sass()).pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(['*.scss'], buildStyles)
+  watch(["*.scss"], buildStyles);
 }
 
-exports.default = series(buildStyles, watchTask)
+exports.default = series(buildStyles, watchTask);
