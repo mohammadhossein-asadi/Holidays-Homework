039835056c62951cfa6469f5eb9d83348e4b4873
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-03-30 01:19:30 +0430

    chnag file

--- 5-&-6-sass/gulpfile.js
+++ 5-&-6-sass/gulpfile.js
@@ -3,7 +3,10 @@ const sass = require("gulp-sass")(require("sass"));
 const purgecss = require("gulp-purgecss");
 
 function buildStyles() {
-  return src("shinobi/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("shinobi/**/*.scss")
+    .pipe(sass())
+    .pipe(purgecss({ content: ["*.html"] }))
+    .pipe(dest("css"));
 }
 
 function watchTask() {
