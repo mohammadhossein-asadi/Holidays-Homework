b7a62bc2dd11d00ff63f45d323bc05e762cb98f5
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-03-30 23:07:48 +0430

    change js

--- 7 & 8  Drag & Drop/2- JavaScript Drag & Drop Sortable List Project/js/script.js
+++ 7 & 8  Drag & Drop/2- JavaScript Drag & Drop Sortable List Project/js/script.js
@@ -22,21 +22,23 @@ let dragStartIndex;
 
 createList();
 
-// * Insert list items into DOM
+const number = [] 
 
+
+// * Insert list items into DOM
 function createList() {
   [...richestPeople].forEach((person, index) => {
     const listItem = document.createElement("li");
 
     listItem.setAttribute("data-index", index);
-
     listItem.innerHTML = `
         <span class="number">${index + 1}</span>
         <div class="draggable" draggable="true">
-        <p class="person-name>${person}</p>
-        <i class="fas fa-grip-lines"></i>
+          <p class="person-name">${person}</p>
+          <i class="fas fa-grip-lines"></i>
         </div>
-    `;
+      `;
+
     listItems.push(listItem);
 
     draggableList.appendChild(listItem);
