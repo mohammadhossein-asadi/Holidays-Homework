7fc75be6f702b485c677766fc018b35919fa6ef0
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-03-30 19:38:52 +0430

    change

---
+++ 7 & 8  Drag & Drop/js/main.js
@@ -0,0 +1,17 @@
+const fill = document.querySelector(".fill");
+
+const empties = document.querySelectorAll(".empty");
+
+// * Fill Listeners
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);
+
+// * Drag Functions
+
+function dragStart() {
+  console.log("start");
+}
+
+function dragEnd() {
+  console.log("end");
+}
