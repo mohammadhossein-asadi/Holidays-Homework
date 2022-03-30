aae27cc0d8200f965491716b8c7ceddb87f1a0d3
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-03-30 22:41:23 +0430

    commit

--- 7 & 8  Drag & Drop/2- JavaScript Drag & Drop Sortable List Project/js/script.js
+++ 7 & 8  Drag & Drop/2- JavaScript Drag & Drop Sortable List Project/js/script.js
@@ -1,4 +1,4 @@
-const dreaggableList = document.getElementById("draggable-list");
+const draggableList = document.getElementById("draggable-list");
 const check = document.getElementById("check");
 
 const richestPeople = [
@@ -13,3 +13,32 @@ const richestPeople = [
   "Michael Bloomberg",
   "Larry Page",
 ];
+
+// * Store ListItems
+
+const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+// * Insert list items into DOM
+
+function createList() {
+  [...richestPeople].forEach((person, index) => {
+    const listItem = document.createElement("li");
+
+    listItem.setAttribute("data-index", index);
+
+    listItem.innerHTML = `
+        <span class="number">${index + 1}</span>
+        <div class="draggable" draggable="true">
+        <p class="person-name>${person}</p>
+        <i class="fas fa-grip-lines"></i>
+        </div>
+    `;
+    listItems.push(listItem);
+
+    draggableList.appendChild(listItem);
+  });
+}
