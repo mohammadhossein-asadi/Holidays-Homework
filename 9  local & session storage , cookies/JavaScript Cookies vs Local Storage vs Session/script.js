96ea8128f75dfa849c269b0eafdeb54fc0121f46
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-04-01 13:05:53 +0430

    commit

--- 9  local & session storage , cookies/JavaScript Cookies vs Local Storage vs Session/script.js
+++ 9  local & session storage , cookies/JavaScript Cookies vs Local Storage vs Session/script.js
@@ -2,8 +2,7 @@
 localStorage.setItem("name", "Mohammad");
 
 // console.log(localStorage.getItem("name"));
-
-localStorage.removeItem("name");
+// localStorage.removeItem("name");
 
 // # Session Storage
 
@@ -11,11 +10,16 @@ sessionStorage.setItem("name", "John");
 // * update value  // Before John => After Bob
 sessionStorage.setItem("name", "Bob");
 
-console.log(sessionStorage.getItem("name"));
+// console.log(sessionStorage.getItem("name"));
 // sessionStorage.removeItem("name");
 
 // # Cookie
 // * Set expire time => expires
-document.cookie = "name=Kyle; expires=";
-+new Date(2020, 0, 1).toUTCString();
+
+document.cookie = "name=Sam; expires=" + new Date(9999, 0, 1).toUTCString();
+
+document.cookie =
+  "lastName=Smith; expires=" + new Date(9999, 0, 1).toUTCString();
+
+console.log(document.cookie);
 
