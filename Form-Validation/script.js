4c48cf9f8848472ece51599e2b1a7994d7b8ad55
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-04-03 11:23:39 +0430

    commit change

--- Form-Validation/script.js
+++ Form-Validation/script.js
@@ -39,7 +39,16 @@ function validationPhone() {
 }
 
 function validationEmail() {
-  const emailError = document.getElementById("contact-email");
+  const email = document.getElementById("contact-email");
 
-  
+  if (email.length == 0) {
+    emailError.innerText = `Email is required`;
+    return false;
+  }
+  if (!email.match(/^[A-Za-z]\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+    emailError.innerText = `Email Invalid`;
+    return false;
+  }
+  emailError.innerHTML = `<i class="fas fa-circle-check"></i>`;
+  return true;
 }
