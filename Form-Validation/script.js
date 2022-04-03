af3610247ef92008bd88c758a95a45ebcdf9b4a2
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-04-03 10:19:13 +0430

    change

--- Form-Validation/script.js
+++ Form-Validation/script.js
@@ -4,5 +4,17 @@ const emailError = document.getElementById("email-error");
 const messageError = document.getElementById("message-error");
 const submitError = document.getElementById("submit-error");
 
+function validationName() {
+  const name = document.getElementById("contact-name");
 
-
+  if (name.lenght == 0) {
+    nameError.innerText = `Name is required`;
+    return false;
+  }
+  if (!name.matches(/^[A-Za-z]*\s{1}[A-Za-z]*&/)) {
+    nameError.innerText = `Write full name`;
+    return false;
+  }
+  nameError.innerText = `Valid`;
+  return true;
+}
