72c0ef375679109b1d94499e6d9122d2e5743616
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-04-03 09:37:54 +0430

    commit change

---
+++ Form-Validation/script.js
@@ -0,0 +1,8 @@
+const nameError = document.getElementById("name-error");
+const phoneError = document.getElementById("phone-error");
+const emailError = document.getElementById("email-error");
+const messageError = document.getElementById("message-error");
+const submitError = document.getElementById("submit-error");
+
+
+
