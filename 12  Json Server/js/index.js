77071f73c7b9540a705c4aac3539ad2ad58ca3df
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-04-05 19:16:02 +0430

    commit

--- 12  Json Server/js/index.js
+++ 12  Json Server/js/index.js
@@ -1,1 +1,11 @@
 // javascript for index.html
+
+const renderPosts = async () => {
+  let url = "http://localhost:3000/posts";
+
+  const res = await fetch(url);
+  const posts = await res.json();
+  console.log(posts);
+};
+
+window.addEventListener("DOMContentLoaded", (e) => renderPosts());
