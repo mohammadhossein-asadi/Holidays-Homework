7826c1fa3457c6a5a0ebe7a812dfca386a83fa83
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-04-05 21:11:43 +0430

    change

--- 12  Json Server/js/details.js
+++ 12  Json Server/js/details.js
@@ -1,4 +1,18 @@
-// javascript for details.html
+const id = new URLSearchParams(window.location.search).get("id");
+const container = document.querySelector(".details");
+const deleteBtn = document.querySelector(".delete");
 
-const id = URLSearchParams(window.location.search).get("id");
+const renderDetails = async () => {
+  const res = await fetch("http://localhost:3000/posts/" + id);
 
+  const post = await res.json();
+
+  const template = `
+    <h1>${post.title}</h1>
+    <p>${post.body}</p>
+  `;
+
+  container.innerHTML = template;
+};
+
+window.addEventListener("DOMContentLoaded", renderDetails);
